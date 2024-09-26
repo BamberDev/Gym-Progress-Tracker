eb1648d4be34e9965609edcb3860eeda165061f8
Bamberek <kabaczek99@gmail.com>
2024-09-26 17:09:17 +0200

    updated metadata in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,65 @@ import { ClerkProvider, SignedIn, UserButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import "./globals.css";
 import { TimerCheckbox } from "@/components/TimerCheckbox";
+import { Metadata } from "next";
 
-export const metadata = {
+export const generateThemeColor = () => ({
+  color: "#303030",
+});
+
+export const generateViewport = () => ({
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+});
+
+export const metadata: Metadata = {
   title: "Gym Progress Tracker",
-  description: "Gym Progress Tracker",
+  description:
+    "Track your gym workouts, set goals, and monitor your fitness progress with ease.",
+  keywords: ["gym", "fitness", "workout", "progress tracker", "exercise"],
+  authors: [{ name: "Kevin" }],
+  creator: "Kevin",
+  publisher: "Gym Progress Tracker",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: "Gym Progress Tracker",
+    description:
+      "Track your gym workouts, set goals, and monitor your fitness progress with ease.",
+    url: "https://gym-progress-tracker-v1.vercel.app/",
+    siteName: "Gym Progress Tracker",
+    images: [
+      {
+        url: "https://gym-progress-tracker-v1.vercel.app/Gym-Logo.png",
+        width: 560,
+        height: 485,
+        alt: "Gym Progress Tracker",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+  category: "Fitness",
 };
+
 export default function RootLayout({
   children,
 }: {
